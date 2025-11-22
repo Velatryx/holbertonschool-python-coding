a4e3c4588e8d6d8cb7db4bfc7ad8947ec5211734
Murx <184812659+Velatryx@users.noreply.github.com>
2025-11-22 22:17:16 +0400

    Refactor docstrings in Square class
    Shortened docstrings for the Square class and its methods.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Sq."""
 
 
 class Square:
-    """Represents a square."""
+    """Represent sq."""
 
     def __init__(self, size):
         """Initialize a new square with a private size."""
