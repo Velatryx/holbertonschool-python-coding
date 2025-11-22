968b9ae608e0ec94d2faca8dec7b58bd01c1ab02
Murx <184812659+Velatryx@users.noreply.github.com>
2025-11-22 22:16:48 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
 
 
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize a new square with a private size."""
         self.__size = size
